8e3218fdbfb48dd83cbe6cfdae33698c2e4f723c
ccarey221 <careyc@union.edu>
2015-12-14 21:41:19 -0500

    Initial Commit

---
+++ FlowMoRefactored/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlowMoRefactored
+//
+//  Created by Conor Carey on 12/14/15.
+//  Copyright © 2015 Conor Carey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
